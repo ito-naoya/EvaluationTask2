c681ee4aff90f8d3f77b527c68044fa444f891f2
ito-naoya <148533254+ito-naoya@users.noreply.github.com>
2024-01-09 17:02:25 +0900

    修正

--- EvaluationTask2/src/DummyFile/InputCheck.java
+++ EvaluationTask2/src/DummyFile/InputCheck.java
@@ -10,21 +10,21 @@ class InputCheck {
 		}		
 		//isNumericやcheckSizeメソッドではInputCheckクラスのstrフィールドを参照していて、元のコードではnullになってしまい、24行目のthis.str.lengthがnullの長さを取得しようとしてエラーとなっていた。
 		//引数のstrの値を再度引数のstrに代入していたため、this.をつけてInputCheckクラスのstrフィールドに代入されるように変更
-		//全角数字の場合は半角数字に変換して代入
+		//全角数字の場合は半角数字に変換して代入（個人練習）
 		this.str = toHalfWidth(str);		
 	}			
 				
 protected boolean isNumeric()				
 {				
 	//空文字をコンソールから入力時に処理が終了しない不具合
-	//元々 ==　で空文字比較をしていたため、オブジェクトとしての比較結果がfalseとなり戻り値のfalseが返されていたなかった
+	//元々 ==　で空文字比較をしていたため、オブジェクトとしての参照先の比較結果がfalseとなり戻り値のfalseが返されていたなかった
 	//isEmptyを使った空文字チェックに切り替えて、空文字が入力された場合はfalseを返し「エラー：数値のみ入力してください。」が出力されるようにした
 	if (this.str.isEmpty()) {			
 		return false;		
 	}
 				
 	for (int i = 0; i < this.str.length(); i++)			
-	{			
+	{
 		char c = this.str.charAt(i);		
 		if (c < '0' || c > '9') {		
 			return false;	
@@ -45,7 +45,7 @@ protected boolean checkSize() {
 	}			
 }				
 
-//全角数字を半角に変換
+//全角数字を半角に変換（個人練習）
 private String toHalfWidth(String str) {
 	  StringBuilder sb = new StringBuilder(str);
 	  for (int i = 0; i < str.length(); i++) {
