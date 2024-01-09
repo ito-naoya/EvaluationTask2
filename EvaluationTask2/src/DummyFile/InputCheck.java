f58e31cea355358356cacd50a85dabd4e344955c
ito-naoya <148533254+ito-naoya@users.noreply.github.com>
2024-01-09 14:43:09 +0900

    コメント修正

--- EvaluationTask2/src/DummyFile/InputCheck.java
+++ EvaluationTask2/src/DummyFile/InputCheck.java
@@ -8,23 +8,24 @@ class InputCheck {
 		if(str == null) {		
 			str = "";	
 		}		
+		//isNumericやcheckSizeメソッドではInputCheckクラスのstrフィールドを参照していて、元のコードではnullになってしまい、24行目のthis.str.lengthがnullの長さを取得しようとしてエラーとなっていた。
 		//引数のstrの値を再度引数のstrに代入していたため、this.をつけてInputCheckクラスのstrフィールドに代入されるように変更
-		//isNumericやcheckSizeメソッドではクラスのstrフィールドを参照していて、元のコードではnullになっていたので24行目のthis.str.lengthがnullの長さを取得しようとしてエラーとなっていた。
-		this.str = str;		
+		//全角数字の場合は半角数字に変換して代入
+		this.str = toHalfWidth(str);		
 	}			
 				
 protected boolean isNumeric()				
 {				
 	if (this.str == "") {			
 		return false;		
-	}			
+	}
 				
 	for (int i = 0; i < this.str.length(); i++)			
 	{			
 		char c = this.str.charAt(i);		
 		if (c < '0' || c > '9') {		
 			return false;	
-		}		
+		}
 	}			
 	return true;			
 }				
@@ -40,4 +41,16 @@ protected boolean checkSize() {
 		return false;		
 	}			
 }				
+
+//全角数字を半角に変換
+private String toHalfWidth(String str) {
+	  StringBuilder sb = new StringBuilder(str);
+	  for (int i = 0; i < str.length(); i++) {
+	    char c = str.charAt(i);
+	    if (0xFF10 <= c && c <= 0xFF19) {
+	      sb.setCharAt(i, (char) (c - 0xFEE0));
+	    }
+	  }
+	  return this.str = sb.toString();
+	}
 }				
